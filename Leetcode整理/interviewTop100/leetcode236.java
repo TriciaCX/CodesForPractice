77147f55eb2ff7d939d486e828b9b189b124e8f3
TriciaCX <tricia_chen@foxmail.com>
2019-07-20 21:59:25 +0800

    leetcode0720

--- Leetcode整理/interviewTop100/leetcode236.java
+++ Leetcode整理/interviewTop100/leetcode236.java
@@ -1,6 +1,115 @@
 package interviewTop100;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import struct.TreeNode;
+
 public class leetcode236
 {
+	/**
+	 * 二叉树的最近公共祖先
+	 * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+	 * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+	 * 例如，给定如下二叉树:  root = [3,5,1,6,2,0,8,null,null,7,4]
+	 */
+	
+	
 
+	/**
+	 * 方法一：递归
+这种方法非常直观。先深度遍历改树。当你遇到节点 p 或 q 时，返回一些布尔标记。该标志有助于确定是否在任何路径中找到了所需的节点。最不常见的祖先将是两个子树递归都返回真标志的节点。它也可以是一个节点，它本身是p或q中的一个，对于这个节点,子树递归返回一个真标志。
+
+让我们看看基于这个想法的形式算法。
+
+算法：
+
+从根节点开始遍历树。
+如果当前节点本身是 p 或 q 中的一个，我们会将变量 mid 标记为 true，并继续搜索左右分支中的另一个节点。
+如果左分支或右分支中的任何一个返回 true，则表示在下面找到了两个节点中的一个。
+如果在遍历的任何点上，左、右或中三个标志中的任意两个变为 true，这意味着我们找到了节点 p 和 q 的最近公共祖先。
+
+	 */
+	class Solution {
+
+	    private TreeNode ans;
+
+	    public Solution() {
+	        // Variable to store LCA node.
+	        this.ans = null;
+	    }
+
+	    private boolean recurseTree(TreeNode currentNode, TreeNode p, TreeNode q) {
+
+	        // If reached the end of a branch, return false.
+	        if (currentNode == null) {
+	            return false;
+	        }
+
+	        // Left Recursion. If left recursion returns true, set left = 1 else 0
+	        int left = this.recurseTree(currentNode.left, p, q) ? 1 : 0;
+
+	        // Right Recursion
+	        int right = this.recurseTree(currentNode.right, p, q) ? 1 : 0;
+
+	        // If the current node is one of p or q
+	        int mid = (currentNode == p || currentNode == q) ? 1 : 0;
+
+
+	        // If any two of the flags left, right or mid become True
+	        if (mid + left + right >= 2) {
+	            this.ans = currentNode;
+	        }
+
+	        // Return true if any one of the three bool values is True.
+	        return (mid + left + right > 0);
+	    }
+
+	    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+	        // Traverse the tree
+	        this.recurseTree(root, p, q);
+	        return this.ans;
+	    }
+	}
+
+	
+	/**
+	 * method2 使用父指针迭代
+	 * 如果每个节点都有父指针，那么我们可以从 p 和 q 返回以获取它们的祖先。在这个遍历过程中，我们得到的第一个公共节点是 LCA 节点。我们可以在遍历树时将父指针保存在字典中。
+	 * 算法：从根节点开始遍历树。在找到 p 和 q 之前，将父指针存储在字典中。
+	 * 一旦我们找到了 p 和 q，我们就可以使用父亲字典获得 p 的所有祖先，并添加到一个称为祖先的集合中。
+	 * 同样，我们遍历节点 q 的祖先。如果祖先存在于为 p 设置的祖先中，这意味着这是 p 和 q 之间的第一个共同祖先（同时向上遍历），因此这是 LCA 节点。
+	 */
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        Map<TreeNode, TreeNode> parent  = new HashMap<TreeNode, TreeNode>();
+        parent.put(root, null);
+        stack.push(root);
+        
+        while(!parent.containsKey(p) || !parent.containsKey(q)) {
+        	TreeNode node = stack.pop();
+        	if(node.left!=null) {
+        		parent.put(node.left, node);
+        		stack.push(node.left);
+        	}
+        	if(node.right!=null) {
+        		parent.put(node.right, node);
+        		stack.push(node.right);
+        	}
+        }
+        
+        Set<TreeNode> ancestors = new HashSet<>();
+        while(p!=null) {
+        	ancestors.add(p);
+        	p = parent.get(p);
+        }
+        
+        while(!ancestors.contains(q))
+        	q = parent.get(q);
+        return q;
+	}
 }
--- Leetcode整理/interviewTop100/leetcode289.java
+++ Leetcode整理/interviewTop100/leetcode289.java
@@ -2,5 +2,75 @@ package interviewTop100;
 
 public class leetcode289
 {
+	/**
+	 * 生命游戏
+	 * 根据百度百科，生命游戏，简称为生命，是英国数学家约翰·何顿·康威在1970年发明的细胞自动机。
+给定一个包含 m × n 个格子的面板，每一个格子都可以看成是一个细胞。每个细胞具有一个初始状态 live（1）即为活细胞， 或 dead（0）即为死细胞。每个细胞与其八个相邻位置（水平，垂直，对角线）的细胞都遵循以下四条生存定律：
+
+如果活细胞周围八个位置的活细胞数少于两个，则该位置活细胞死亡；
+如果活细胞周围八个位置有两个或三个活细胞，则该位置活细胞仍然存活；
+如果活细胞周围八个位置有超过三个活细胞，则该位置活细胞死亡；
+如果死细胞周围正好有三个活细胞，则该位置死细胞复活；
+根据当前状态，写一个函数来计算面板上细胞的下一个（一次更新后的）状态。下一个状态是通过将上述规则同时应用于当前状态下的每个细胞所形成的，其中细胞的出生和死亡是同时发生的。
+	 */
+
+	
+	/**
+	 * 1、遍历二维数组，然后改变每个点的状态； 
+	 * 2、遍历取当前位置周围8个细胞，同时记录活细胞数量。如果x坐标或y坐标超出边界，则不记录； （important）把即将死亡的 活细胞 置为 2， 把即将复活的 死细胞 ，置为 -1； 则在判断 当前细胞 >0 时当前为活细胞，<= 0 当前为死细胞；
+	 * 3、根据题目要求，通过活细胞数量来改变该点的状态；
+	 * 4、最后，再遍历数组，将2 还原为 0，将 -1 还原为 1；
+	 */
+	
+	public void gameOfLife(int[][] board) {
+		//遍历数组
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                changeBorder(board,i,j);
+            }
+        }
+        
+        //恢复二维数组
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                if(board[i][j] == -1){  //复活的死细胞
+                    board[i][j] = 1;
+                }
+                if(board[i][j] == 2){   //死亡的活细胞
+                    board[i][j] = 0;
+                }
+            }
+        }
+        
+    }
+
+    public void changeBorder(int[][] board,int x, int y){
+        int row = board.length;
+        int col = board[0].length;
+
+        int total = 0; // 该细胞周围活细胞的数量
+
+        for(int i= x - 1; i <= x + 1 ; i++){
+            for(int j= y - 1; j <= y + 1; j++){
+                if(i == x && j == y){
+                    continue;
+                }
+                if(i >= 0 && i < row && j >= 0 && j < col){
+                    if(board[i][j] > 0){
+                        total ++;
+                    }
+                }
+            }
+        }
+        if(board[x][y] > 0){
+            if(total < 2 || total > 3){
+                board[x][y] = 2;
+            }
+        }
+        if(board[x][y] <= 0 && total == 3){
+            board[x][y] = -1;
+        }
+
+    }
 
 }
